617ee81193480858a38963dc8fef9e6ce579308d
daria25D <dasha_domracheva@mail.ru>
2019-03-06 17:21:03 +0300

    mpi_double_complex

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -10,18 +10,18 @@
 typedef std::complex<double> complexd;
 using namespace std;
 
-long long power(long long base, long long deg) {
+long long power(int base, int deg) {
 	long long res = 1;
 	if (base == 2) {
-		for (long long i = 0; i < deg; i++)
+		for (int i = 0; i < deg; i++)
 			res <<= 1;
 	} else {
-		for (long long i = 0; i < deg; i++)
+		for (int i = 0; i < deg; i++)
 			res *= base;
 	}
 	return res;
 }
-complexd * quantum(complexd * a, long long n, complexd ** U, long long k) {
+complexd * quantum(complexd * a, int n, complexd ** U, int k) {
     /**
     TODO
     messages between processes (send parts of array a)
@@ -60,29 +60,28 @@ complexd * quantum(complexd * a, long long n, complexd ** U, long long k) {
                     rank2 = j;
                 if (rank1 != 0 && rank2 != 0) break;
             }
-            complexd a_idx;
-            double a_idx1_real, a_idx1_imag, a_idx2_real, a_idx2_imag;
+            complexd a_idx, a_idx_swap;
             if (rank1 != rank) { //need to send a[idx2] and receive a[idx1]
-                a_idx2_real = a[idx2 - rank * size_of_a].real();
-                a_idx2_imag = a[idx2 - rank * size_of_a].imag();
+                a_idx_swap = a[idx2 - rank*size_of_a];
                 MPI_Status status;
-                MPI_Send(&a_idx2_real, 1, MPI_DOUBLE, rank1, 0, MPI_COMM_WORLD);
-                MPI_Recv(&a_idx1_real, 1, MPI_DOUBLE, rank1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Send(&a_idx2_imag, 1, MPI_DOUBLE, rank1, 0, MPI_COMM_WORLD);
-                MPI_Recv(&a_idx1_imag, 1, MPI_DOUBLE, rank1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                a_idx.real(a_idx1_real);
-                a_idx.imag(a_idx1_imag);
+                MPI_Send(&a_idx_swap, 1, MPI_DOUBLE_COMPLEX, rank1, 0, MPI_COMM_WORLD);
+                MPI_Recv(&a_idx, 1, MPI_DOUBLE_COMPLEX, rank1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                //MPI_Send(&a_idx2_imag, 1, MPI_DOUBLE, rank1, 0, MPI_COMM_WORLD);
+                //MPI_Recv(&a_idx1_imag, 1, MPI_DOUBLE, rank1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                //a_idx.real(a_idx1_real);
+                //a_idx.imag(a_idx1_imag);
                 b[i] = U[idx_ik][0] * a_idx + U[idx_ik][1] * a[idx2 - rank * size_of_a];
             } else if (rank2 != rank) { //need to send a[idx1] and receive a[idx2]
-                a_idx1_real = a[idx1 - rank * size_of_a].real();
-                a_idx1_imag = a[idx1 - rank * size_of_a].imag();
+                //a_idx1_real = a[idx1 - rank * size_of_a].real();
+                //a_idx1_imag = a[idx1 - rank * size_of_a].imag();
+                a_idx_swap = a[idx1 - rank*size_of_a];
                 MPI_Status status;
-                MPI_Recv(&a_idx2_real, 1, MPI_DOUBLE, rank2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Send(&a_idx1_real, 1, MPI_DOUBLE, rank2, 0, MPI_COMM_WORLD);
-                MPI_Recv(&a_idx2_imag, 1, MPI_DOUBLE, rank2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Send(&a_idx1_imag, 1, MPI_DOUBLE, rank2, 0, MPI_COMM_WORLD);
-                a_idx.real(a_idx2_real);
-                a_idx.imag(a_idx2_imag);
+                MPI_Recv(&a_idx, 1, MPI_DOUBLE_COMPLEX, rank2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Send(&a_idx_swap, 1, MPI_DOUBLE_COMPLEX, rank2, 0, MPI_COMM_WORLD);
+                //MPI_Recv(&a_idx2_imag, 1, MPI_DOUBLE, rank2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                //MPI_Send(&a_idx1_imag, 1, MPI_DOUBLE, rank2, 0, MPI_COMM_WORLD);
+                //a_idx.real(a_idx2_real);
+                //a_idx.imag(a_idx2_imag);
                 b[i] = U[idx_ik][0] * a[idx1 - rank * size_of_a] + U[idx_ik][1] * a_idx;
             } else { // no messages needed
 		        b[i] = U[idx_ik][0] * a[idx1 - rank * size_of_a] + U[idx_ik][1] * a[idx2 - rank * size_of_a]; 
@@ -92,7 +91,7 @@ complexd * quantum(complexd * a, long long n, complexd ** U, long long k) {
 	return b;
 }
 
-complexd * generate(long long n) {
+complexd * generate(int n) {
     /*
     TODO
     partition of array a between processes
@@ -121,12 +120,43 @@ complexd * generate(long long n) {
 	return a;
 }
 
+// complexd * read_array(MPI_File f, int n) {
+//     double a_real = 0, a_imag = 0;
+
+// }
+
 int main(int argc, char ** argv) {
-	long long n, k;
+    // ./task2 n k gen output
+    // or
+    // ./task2 file_in file_out
+	int n, k;
     bool gen_flag = false;
-    if (argc == 4 && argv[3] == "gen") gen_flag = true;
-	n = atoi(argv[1]);
-	k = atoi(argv[2]);
+    if (argc != 2 && argc != 5) {
+        cout << "Not enough arguments\n";
+        return -1;
+    }
+    if (argc == 5 && strcmp(argv[3], "gen") == 0) { //generate mod
+        gen_flag = true;
+	    n = atoi(argv[1]);
+	    k = atoi(argv[2]);
+    }   
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    double timer1 = 0.0;
+    complexd * a;
+	if (gen_flag) {
+        timer1 = MPI_Wtime();
+	    a = generate(n);
+        timer1 = MPI_Wtime() - timer1;
+    } else {
+        //read from file
+        MPI_File f;
+        MPI_File_open(MPI_COMM_WORLD, argv[3], MPI_MODE_RDONLY, MPI_INFO_NULL, &f);
+        MPI_File_read_all(f, &n, 1, MPI_INT, MPI_STATUS_IGNORE);
+        MPI_File_read_all(f, &k, 1, MPI_INT, MPI_STATUS_IGNORE);
+    }
 	complexd ** U = new complexd*[2];
 	U[0] = new complexd[2];
 	U[1] = new complexd[2];
@@ -134,33 +164,24 @@ int main(int argc, char ** argv) {
 	U[0][1] = 1 / sqrt(2);
 	U[1][0] = 1 / sqrt(2);
 	U[1][1] = -1 / sqrt(2);
-    MPI_Init(&argc, &argv);
-    double timer1 = 0.0;
-	if (gen_flag) timer1 = MPI_Wtime();
-	complexd * a = generate(n);
-    if (gen_flag) timer1 = MPI_Wtime() - timer1;
+
 	double timer2 = MPI_Wtime();
 	complexd * b = quantum(a, n, U, k);
 	timer2 = MPI_Wtime() - timer2;
-	//long long size = power(2, n);
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	long long size_array = power(2, n) / size;
 
-	ofstream f("vectors.txt");
     /**
     TODO
     MPI file usage
     */
-	// for (long long i = 0; i < size; i++) {
-	// 	f << a[i] << "\n";
-	// }
-	// f << endl;
-	// for (long long i = 0; i < size; i++) {
-	// 	f << b[i] << "\n";
-	// }
-
-	if (rank == 0) cout << timer1 + timer2 << endl;
+    if (gen_flag && (argv[4], "no") != 0) {
+        //file output in generation mode
+    } else if (argc == 3 && strcmp(argv[2], "no") != 0) {
+        //file output in file mode
+    } else {
+        //no output
+    }
+	if (rank == 0) cout << timer1 << "\n" << timer2 << endl;
 	delete a;
 	delete b;
     MPI_Finalize();
