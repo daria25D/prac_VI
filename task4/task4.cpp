1b84eb50d211fea7a7261c41ef5c590dbddc26e2
daria25D <dasha_domracheva@mail.ru>
2019-04-17 22:11:37 +0300

    lint

--- task4/task4.cpp
+++ task4/task4.cpp
@@ -1,63 +1,57 @@
-#include "quantum.h"
-#include <cstring>
+/* "Copyright 2019 <Copyright Owner>" */
+#include <string>
 #include <fstream>
+#include "./quantum.h"
 
 int main(int argc, char ** argv) {
-	string type = string(argv[1]);
-		MPI_Init(&argc, &argv);
-	//fstream f("out.txt", ios::out);
-	if (type == "Adamar") {
-		//Adamar n k
-		int n = atoi(argv[2]);
-		int k = atoi(argv[3]);
-		int rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		complexd * a = generate(n);
-		complexd * b = quantum_Adamar(a, n, k);
-		File_MPI_Write(b, power(2, n), k, argv[4], rank);
-		delete[] a;
-		delete[] b;
-	} else if (type == "nAdamar") {
-		//nAdamar n
-		int n = atoi(argv[2]);
-		int rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		complexd * a = generate(n);
-		complexd * b = quantum_nAdamar(a, n);
-		File_MPI_Write(b, power(2, n), 0, argv[3], rank);
-		delete[] a;
-		delete[] b;
-
-	} else if (type == "Rotate") {
-		//Rotate n k phi
-		int n = atoi(argv[2]);
-		int k = atoi(argv[3]);
-		double phi = atof(argv[4]);
-		int rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		complexd * a = generate(n);
-		complexd * b = quantum_Rotate(a, n, k, phi);
-		File_MPI_Write(b, power(2, n), k, argv[5], rank);
-		delete[] a;
-		delete[] b;
-
-	} else if (type == "Not") {
-		//Not n k
-		int n = atoi(argv[2]);
-		int k = atoi(argv[3]);
-		int rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		complexd * a = generate(n);
-		complexd * b = quantum_Not(a, n, k);
-		File_MPI_Write(b, power(2, n), k, argv[4], rank);
-		File_MPI_Write(a, power(2, n), k, argv[5], rank);
-		delete[] a;
-		delete[] b;
-
-	} else {
-
-	}
-	MPI_Finalize();
-	//f.close();
-	return 0;
+    string type = string(argv[1]);
+    MPI_Init(&argc, &argv);
+    if (type == "Adamar") {
+        // Adamar n k
+        int n = atoi(argv[2]);
+        int k = atoi(argv[3]);
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        complexd * a = generate(n);
+        complexd * b = quantum_Adamar(a, n, k);
+        File_MPI_Write(b, power(2, n), k, argv[4], rank);
+        delete[] a;
+        delete[] b;
+    } else if (type == "nAdamar") {
+        // nAdamar n
+        int n = atoi(argv[2]);
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        complexd * a = generate(n);
+        complexd * b = quantum_nAdamar(a, n);
+        File_MPI_Write(b, power(2, n), 0, argv[3], rank);
+        delete[] a;
+        delete[] b;
+    } else if (type == "Rotate") {
+        // Rotate n k phi
+        int n = atoi(argv[2]);
+        int k = atoi(argv[3]);
+        double phi = atof(argv[4]);
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        complexd * a = generate(n);
+        complexd * b = quantum_Rotate(a, n, k, phi);
+        File_MPI_Write(b, power(2, n), k, argv[5], rank);
+        delete[] a;
+        delete[] b;
+    } else if (type == "Not") {
+        // Not n k
+        int n = atoi(argv[2]);
+        int k = atoi(argv[3]);
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        complexd * a = generate(n);
+        complexd * b = quantum_Not(a, n, k);
+        File_MPI_Write(b, power(2, n), k, argv[4], rank);
+        File_MPI_Write(a, power(2, n), k, argv[5], rank);
+        delete[] a;
+        delete[] b;
+    }
+    MPI_Finalize();
+    return 0;
 }
