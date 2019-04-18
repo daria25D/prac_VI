7362deb79808634f3c8d0e7ac2b7b862d18595c2
daria25D <dasha_domracheva@mail.ru>
2019-04-18 09:38:29 +0300

    lint for tests

--- task4/test_not.cpp
+++ task4/test_not.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
 #include <complex>
 
-long long power(long long base, long long deg) {
-	long long res = 1;
-	if (base == 2) {
-		for (long long i = 0; i < deg; i++)
-			res <<= 1;
-	} else {
-		for (long long i = 0; i < deg; i++)
-			res *= base;
-	}
-	return res;
+typedef std::complex<double> complexd;
+using namespace std;
+
+uint64_t power(uint64_t base, uint64_t deg) {
+    uint64_t res = 1;
+    if (base == 2) {
+        for (uint64_t i = 0; i < deg; i++)
+            res <<= 1;
+    } else {
+        for (uint64_t i = 0; i < deg; i++)
+            res *= base;
+    }
+    return res;
 }
 
 int main(int argc, char **argv) {
     int n, k;
     n = atoi(argv[2]);
     k = atoi(argv[3]);
-    long long size;
+    uint64_t size;
     FILE *f = fopen(argv[1], "rb");
     if (fread(&size, sizeof(size), 1, f) != 1) {
-        std::cout << 1 << std::endl;
+        cout << 1 << endl;
         exit(1);
     }
     if (fread(&k, sizeof(k), 1, f) != 1) {
-        std::cout << 2 << std::endl;
+        cout << 2 << endl;
         exit(1);
     }
-    std::complex<double> *a = new std::complex<double>[size];
-    std::complex<double> *b = new std::complex<double>[size];
-    for (long long i = 0; i < size; i++) {
+    complexd *a = new complexd[size];
+    complexd *b = new complexd[size];
+    for (uint64_t i = 0; i < size; i++) {
         if (fread(&a[i], sizeof(a[0]), 1, f) != 1) {
-            //exit(1);
+            // exit
         }
     }
     fclose(f);
-
     int q = -1;
     uint64_t tmpn = size;
     while (tmpn) {
@@ -47,13 +49,11 @@ int main(int argc, char **argv) {
     for (uint64_t i = 0; i < size; ++i) {
         b[i] = a[i ^ bit];
     }
-
     f = fopen(argv[4], "wb");
     fwrite(&size, sizeof(size), 1, f);
     fwrite(&k, sizeof(k), 1, f);
     fwrite(b, sizeof(b[0]), size, f);
     fclose(f);
-
     delete [] a;
     delete [] b;
 }
